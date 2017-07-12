b3246b7c843f506f8081189ab96a417eece653c7
Libe <liberatoalan@hotmail.com>
2017-07-11 20:34:05 -0600

    Initial Commit

---
+++ SpeedRead/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SpeedRead
+//
+//  Created by libe on 7/11/17.
+//  Copyright © 2017 Liberato Aguilar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
